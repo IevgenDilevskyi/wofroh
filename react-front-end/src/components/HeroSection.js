768e44dc77a23df1178a607a67c60a1ed6f6bf0f
Ievgen Dilevskyi <e.dilevskiy@gmail.com>
2021-05-27 15:34:10 -0600

    cleanup HeroSection.js

--- react-front-end/src/components/HeroSection.js
+++ react-front-end/src/components/HeroSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../App.css";
 import { Button } from "./show/Button";
 import "./HeroSection.css";
-import { Link } from "react-router-dom";
+
 
 function HeroSection() {
   return (
